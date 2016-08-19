9eb2fbd1463847c251cc7887431aee27935d390e
vangheem <vangheem@gmail.com>
2016-08-19 14:40:35 -0500

    add blob store example

--- src/zodb_dynamicstorage/scripts/client.py
+++ src/zodb_dynamicstorage/scripts/client.py
@@ -1,5 +1,7 @@
+from ZODB.blob import Blob
 from persistent import Persistent
 import transaction
+from tempfile import mkdtemp
 
 
 class Foobar(Persistent):
@@ -11,21 +13,35 @@ if __name__ == '__main__':
     from ZODB.DB import DB
 
     # default storage '1'
-    clientstorage = ClientStorage(('127.0.0.1', 8100), '1')
+    clientstorage = ClientStorage(
+        ('127.0.0.1', 8100), '1',
+        blob_dir=mkdtemp())
     db = DB(clientstorage)
 
     conn = db.open()
     root = conn.root()
 
     root['foobar'] = Foobar()
+    blob = Blob()
+    bfile = blob.open('w')
+    bfile.write('foobar')
+    bfile.close()
+    root['foobarblob'] = blob
     transaction.commit()
 
     # use anthor storage name...
-    clientstorage = ClientStorage(('127.0.0.1', 8100), 'foobar')
+    clientstorage = ClientStorage(
+        ('127.0.0.1', 8100), 'foobar',
+        blob_dir=mkdtemp())
     db = DB(clientstorage)
 
     conn = db.open()
     root = conn.root()
 
     root['foobar'] = Foobar()
+    blob = Blob()
+    bfile = blob.open('w')
+    bfile.write('foobar')
+    bfile.close()
+    root['foobarblob'] = blob
     transaction.commit()
